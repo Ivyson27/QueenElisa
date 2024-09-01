665b4d4df13649abde1fc18d0e74fd16d6a34abc
Ivyson27 <Kamogeloelia27@gmail.com>
2024-09-01 22:22:46 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 module.exports = {
-  SESSION_ID: '',
+  SESSION_ID: 'QUEENELISA;;;==wb0gEa3tmdfRmZvxmT0QlRxVHVZ1EZwhTeCFkckp1ZZFEbwdFarh0TaR1YjImYKhEb5YkTpwd=55458028672',
   ALIVE_REACT: '👋',
   ALIVE_MESSAGE: `*Hello @name [ And @gname @members Number of Members ] I am Alive Now !*
 
